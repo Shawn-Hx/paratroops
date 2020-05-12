41a461e92a805c19548331e04d98c305e9b27e8a
weiyx <weiyuxuan@baidu.com>
2020-05-12 09:00:30 +0800

    first commit

---
+++ src/main/java/com/paratroops/App.java
@@ -0,0 +1,13 @@
+package com.paratroops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
